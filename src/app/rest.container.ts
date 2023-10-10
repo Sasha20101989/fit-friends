90edc1198dc13945f14a17e20c4cf45427317db7
Лига А <feliugin@gmail.com>
2023-10-10 18:22:07 +0300

    feat: add mongo service

--- src/app/rest.container.ts
+++ src/app/rest.container.ts
@@ -2,11 +2,14 @@ import { Container } from 'inversify';
 
 import type { LoggerInterface } from '../core/logger/logger.interface.js';
 import type { ConfigInterface } from '../core/config/config.interface.js';
+import type { DatabaseClientInterface } from '../core/database-client/mongo-client.interface.js';
+
 import { AppComponent } from '../types/app-component.enum.js';
 import PinoService from '../core/logger/pino.service.js';
 import RestApplication from './rest.js';
 import { RestSchema } from '../core/config/rest.schema.js';
 import ConfigService from '../core/config/config.service.js';
+import MongoClientService from '../core/database-client/database-client.service.js';
 
 export function createRestApplicationContainer() {
   const restApplicationContainer = new Container();
@@ -14,6 +17,7 @@ export function createRestApplicationContainer() {
   restApplicationContainer.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication).inSingletonScope();
   restApplicationContainer.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService).inSingletonScope();
   restApplicationContainer.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService).inSingletonScope();
+  restApplicationContainer.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface).to(MongoClientService).inSingletonScope();
 
   return restApplicationContainer;
 }
