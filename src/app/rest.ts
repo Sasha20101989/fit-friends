1500d2239ce34fb88a7e6488ff4c3843dbadbbc8
Лига А <feliugin@gmail.com>
2023-10-10 17:43:15 +0300

    feat: add rest container

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,12 +1,15 @@
 import express, { Express } from 'express';
 import { inject, injectable } from 'inversify';
 
+import type { LoggerInterface } from '../core/logger/logger.interface.js';
+import { AppComponent } from '../types/app-component.enum.js';
+
 @injectable()
 export default class RestApplication {
   private expressApplication: Express;
 
   constructor(
-
+    @inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
   ) {
     this.expressApplication = express();
   }
