03f36b4a29ced70dec501c5148eb0023aeb4db0a
Лига А <feliugin@gmail.com>
2023-10-28 14:46:45 +0300

    Update rest.container.ts

--- src/app/rest.container.ts
+++ src/app/rest.container.ts
@@ -10,6 +10,12 @@ import RestApplication from './rest.js';
 import { RestSchema } from '../core/config/rest.schema.js';
 import ConfigService from '../core/config/config.service.js';
 import MongoClientService from '../core/database-client/database-client.service.js';
+import { RabbitClientInterface } from '../core/rabit-client/rabit-client.interface.js';
+import RabbitClientService from '../core/rabit-client/rabit-client.service.js';
+import ClientConsumer from '../core/rabit-client/consumer/client-consumer.js';
+import { ClientConsumerInterface } from '../core/rabit-client/consumer/client-consumer.interface.js';
+import ClientProducer from '../core/rabit-client/producer/client-producer.js';
+import { ClientProducerInterface } from '../core/rabit-client/producer/client-producer.interface.js';
 
 export function createRestApplicationContainer() {
   const restApplicationContainer = new Container();
@@ -18,6 +24,9 @@ export function createRestApplicationContainer() {
   restApplicationContainer.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService).inSingletonScope();
   restApplicationContainer.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService).inSingletonScope();
   restApplicationContainer.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface).to(MongoClientService).inSingletonScope();
+  restApplicationContainer.bind<RabbitClientInterface>(AppComponent.RabbitClientInterface).to(RabbitClientService).inSingletonScope();
+  restApplicationContainer.bind<ClientConsumerInterface>(AppComponent.ClientConsumerInterface).to(ClientConsumer).inSingletonScope();
+  restApplicationContainer.bind<ClientProducerInterface>(AppComponent.ClientProducerInterface).to(ClientProducer).inSingletonScope();
 
   return restApplicationContainer;
 }
