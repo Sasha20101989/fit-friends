603e87269792d5bcf80c90990f74230f0bf6ded8
Лига А <feliugin@gmail.com>
2023-11-07 15:24:29 +0300

    feat: add log dir

--- src/core/logger/pino.service.ts
+++ src/core/logger/pino.service.ts
@@ -1,14 +1,31 @@
 import { Logger, pino } from 'pino';
 import { injectable } from 'inversify';
+import path from 'path';
+import fs from 'fs';
 
 import type { LoggerInterface } from './logger.interface.js';
+
 
 @injectable()
 export default class PinoService implements LoggerInterface {
   private readonly logger: Logger;
 
   constructor() {
-    this.logger = pino();
+    const currentDirectory = process.cwd();
+
+    const logDirectory = path.join(currentDirectory, './log');
+    if (!fs.existsSync(logDirectory)) {
+      fs.mkdirSync(logDirectory);
+    }
+
+    const today = new Date();
+    const dateString = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    const logFilePath = path.join(logDirectory, `logfile_${dateString}.log`);
+
+    this.logger = pino({
+      level: 'debug',
+    }, pino.destination(logFilePath));
+
     this.logger.info('Logger created');
   }
 
