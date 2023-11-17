bb4fdbf4884a9fda498df6cdee4104d0928d006f
Лига А <feliugin@gmail.com>
2023-11-17 21:03:46 +0300

    init react app

--- backend/src/modules/user/validators/is-file-size-valid.validator.ts
+++ backend/src/modules/user/validators/is-file-size-valid.validator.ts
@@ -1,13 +1,13 @@
 import { registerDecorator, ValidationOptions } from 'class-validator';
 
-interface FileWithSize {
+export interface FileWithSize {
   size: number;
 }
 
 const isFileSizeValid = (sizeLimit: number) => (value: FileWithSize) => value && value.size <= sizeLimit;
 
 export function IsFileValidSize(sizeLimit: number, validationOptions?: ValidationOptions) {
-  return function (object: FileWithSize, propertyName: string) {
+  return function (object: Record<string, any>, propertyName: string) {
     registerDecorator({
       name: 'isFileValidSize',
       target: object.constructor,
